45e690801ecc905a0642c55577f18d8e5576d96c
luhailong <lhpcpp6@163.com>
2025-08-08 22:00:34 +0800

    add room

--- apps/user/internal/logic/user/loginlogic.go
+++ apps/user/internal/logic/user/loginlogic.go
@@ -3,48 +3,46 @@ package userlogic
 import (
 	"context"
 	"errors"
-	"github.com/golang-jwt/jwt/v4"
-	"time"
-
-	"fish-game/apps/user/fish-game/apps/user/user"
 	"fish-game/apps/user/internal/svc"
-
+	"fish-game/apps/user/user"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/zeromicro/go-zero/core/logx"
+	"time"
 )
 
+var jwtSecret = "secret123"
+
 type LoginLogic struct {
+	logx.Logger
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
-	logx.Logger
 }
 
 func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
 	return &LoginLogic{
+		Logger: logx.WithContext(ctx),
 		ctx:    ctx,
 		svcCtx: svcCtx,
-		Logger: logx.WithContext(ctx),
 	}
 }
 
-var jwtSecret = "your-secret-key"
-
 func (l *LoginLogic) Login(in *user.LoginRequest) (*user.LoginResponse, error) {
-	if in.Username == "admin" && in.Password == "123456" {
-		uid := int64(1001)
-
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-			"uid": uid,
-			"exp": time.Now().Add(time.Hour * 24).Unix(),
-		})
-		tokenStr, err := token.SignedString([]byte(jwtSecret))
-		if err != nil {
-			return nil, err
-		}
-
-		return &user.LoginResponse{
-			Uid:   uid,
-			Token: tokenStr,
-		}, nil
+	if in.Username != "admin" || in.Password != "123456" {
+		return nil, errors.New("用户名或密码错误")
 	}
-	return nil, errors.New("用户名或密码错误")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"uid": 1001,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenStr, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	return &user.LoginResponse{
+		Uid:   1001,
+		Token: tokenStr,
+	}, nil
 }
